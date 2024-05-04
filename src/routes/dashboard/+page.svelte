d8e257322d2c7c399a04c803b7a201bd54f65991
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-05-04 14:37:29 -0400

    Update modal + add functionality

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Item from '$lib/components/Item.svelte';
-	import { onMount } from 'svelte';
+	import { SvelteComponent, onMount, type ComponentType } from 'svelte';
 
 	const toBase64: (file: File) => any = file => new Promise((resolve, reject) => {
 		const reader = new FileReader();
@@ -11,37 +11,47 @@
 
 	export let data;
 
-	onMount(() => {
-		document.getElementsByTagName('button')[0].addEventListener('click', async () => {
-			// @ts-ignore
-			let a = document.getElementsByTagName('input')[0].files[0];
-			console.log(a)
-			// TODO: save image to database
-			const data = new FormData();
-			// @ts-ignore
+	// onMount(() => {
+	// 	document.getElementsByTagName('button')[0].addEventListener('click', async () => {
+	// 		// @ts-ignore
+	// 		let a = document.getElementsByTagName('input')[0].files[0];
+	// 		console.log(a)
+	// 		// TODO: save image to database
+	// 		const data = new FormData();
+	// 		// @ts-ignore
 
-			data.append('image', await toBase64(a));
-			const response = await fetch(`/api/add-item`, {
-				method: 'POST',
-				body: data
-			})
-			console.log(await response.text())
-		})
-	})
+	// 		data.append('image', await toBase64(a));
+	// 		const response = await fetch(`/api/add-item`, {
+	// 			method: 'POST',
+	// 			body: data
+	// 		})
+	// 		console.log(await response.text())
+	// 	})
+	// });
+
+	import { writable } from 'svelte/store';
+	import Modal, { bind } from 'svelte-simple-modal';
+	import Popup from './Popup.svelte';
+	const modal = writable<ComponentType | null>(null);
+	const showModal = () => modal.set(bind(Popup, { message: 'Surprise' }));
+
 </script>
 
 <input type="file"/>
-<div class="p-3">
-	<div class="flex mb-2">
-		<button type="button" class="btn variant-filled mr-auto"><span class="font-extrabold text-xl pr-1">+</span>add</button>
-		<button class="btn variant-filled ml-auto">⧩</button>
+<Modal show={$modal}>
+
+	<div class="p-3">
+		<div class="flex mb-2">
+			<button on:click={showModal} type="button" class="btn variant-filled mr-auto"><span class="font-extrabold text-xl pr-1">+</span>add</button>
+			<button class="btn variant-filled ml-auto">⧩</button>
+		</div>
+		<div class="grid grid-cols-4 gap-6">
+			<!-- {#each {length: 7} as _, i}
+				<Item item={{name: "testName", type:"testType", calories:1000, healthy:false, quantity: 2}} />
+			{/each} -->
+			{#each data.fridge as item}
+				<Item item={item}>{item.name}</Item>
+			{/each}
+		</div>
 	</div>
-	<div class="grid grid-cols-4 gap-6">
-		<!-- {#each {length: 7} as _, i}
-			<Item item={{name: "testName", type:"testType", calories:1000, healthy:false, quantity: 2}} />
-		{/each} -->
-		{#each data.fridge as item}
-			<Item item={item}>{item.name}</Item>
-		{/each}
-	</div>
-</div>
+</Modal>
---
+++ src/routes/dashboard/Popup.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+    export let message = 'Hi';
+    
+    let stream: MediaStream;
+    let videoRef: HTMLVideoElement;
+    let canvasRef: HTMLCanvasElement;
+
+
+    let width = 0;    // We will scale the photo width to this
+    let height = 0;     // This will be computed based on the input stream
+    async function getStream() {
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            });
+            videoRef.srcObject = stream;
+        } catch (err) {
+            console.error(err);
+        }
+        console.log(stream.getTracks()[0])
+
+
+        videoRef.addEventListener(
+            "canplay",
+            (ev) => {
+                // if (!streaming) {
+                width = videoRef.offsetWidth;
+                height = videoRef.offsetHeight;
+                // height = videoRef.videoHeight / (videoRef.videoWidth / width);
+
+                // // Firefox currently has a bug where the height can't be read from
+                // // the video, so we will make assumptions if this happens.
+
+                // if (isNaN(height)) {
+                //     height = width / (4 / 3);
+                // }
+
+                videoRef.setAttribute("width", width.toString());
+                videoRef.setAttribute("height", height.toString());
+                canvasRef.setAttribute("width", width.toString());
+                canvasRef.setAttribute("height", height.toString());
+                // }
+            },
+            false,
+            );
+    }
+
+    async function stopStream() {
+        function clearphoto() {
+            const context = canvasRef.getContext("2d") as CanvasRenderingContext2D;
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvasRef.width, canvasRef.height);
+
+            // const data = canvasRef.toDataURL("image/png");
+            // photo.setAttribute("src", data);
+        }
+
+        function takepicture() {
+            const context = canvasRef.getContext("2d") as CanvasRenderingContext2D;
+            if (width && height) {
+                canvasRef.width = width;
+                canvasRef.height = height;
+                context.drawImage(videoRef, 0, 0, width, height);
+                videoRef.classList.add('hidden')
+
+                const base64Url = canvasRef.toDataURL("image/png");
+                async function request() {
+                    const data = new FormData();
+                    data.append('image', base64Url);
+                    const response = await fetch(`/api/add-item`, {
+                        method: 'POST',
+                        body: data
+                    })
+                    console.log(await response.text())
+                }
+                request()
+            } else {
+                clearphoto();
+            }
+        }
+
+        takepicture();
+
+        stream.getTracks().forEach(track => track.stop());
+        videoRef.srcObject = null;
+    }
+
+    getStream();
+
+</script>
+  
+<section class="container mx-auto px-4">
+    <!-- <button class="rounded-sm bg-slate-600 text-white px-4 py-2" on:click={getStream}>Start Stream</button> -->
+    <button class="rounded-sm bg-red-600 text-white px-4 py-2" on:click={stopStream}>Take</button>
+    <video class="mt-4 rounded-sm " width="640" height="480" autoplay={true} bind:this={videoRef} />
+    <canvas id="canvas" class="d-none" bind:this={canvasRef}></canvas>
+    
+</section>
