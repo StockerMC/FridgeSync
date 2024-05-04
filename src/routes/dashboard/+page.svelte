23a4d71cb96c9aad8c1b26506369325cb9c476f7
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-05-04 17:35:53 -0400

    Photo

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -59,7 +59,7 @@
 				<Item item={{name: "testName", type:"testType", calories:1000, healthy:false, quantity: 2}} />
 			{/each} -->
 			{#each data.fridge.filter((item) => item.name.toLowerCase().includes(searchquery.toLowerCase().trim()) || item.type.toLowerCase().includes(searchquery.toLowerCase().trim())) as item}
-				<Item item={item}>{item.name}</Item>
+				<Item item={item} photo={data.photos?.filter(photo => photo.id == item.id)[0]}>{item.name}</Item>
 			{/each}
 		</div>
 	</div>
