ad1fd91ef240fd957d8dca758d2d2e00f25d2360
Skilled5041 <skilled5041@gmail.com>
2024-05-04 13:09:04 -0400

    Landing page part 1 finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,25 @@
 		<!--TODO: Change this-->
 		<h1 class="text-7xl gradient-heading font-extrabold leading-normal">FridgeSync: Instant Inventory!</h1>
 		<div class="flex gap-12">
-			<button type="button" class="btn variant-filled">Get Started</button>
+			<button type="button" class="btn variant-filled">
+				<a href="/dashboard">Get Started</a>
+			</button>
 			<button type="button" class="btn variant-ghost">Learn More</button>
 		</div>
 	</div>
 	<div>
 		<img alt="fridge" src={fridge} draggable="false">
 	</div>
+</div>
+<div class="p-24">
+	<h1 class="text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, distinctio dolor dolorem error
+		fugit impedit in
+		labore pariatur tempora vero! Accusantium ad adipisci aliquam est itaque. Architecto corporis dicta dignissimos
+		doloribus ea eius et eum labore laborum libero molestias obcaecati odit, porro quaerat quisquam reprehenderit
+		unde veniam voluptatibus? Alias amet assumenda distinctio dolor doloremque eos ex fugit iure magni molestias,
+		nam nisi nostrum perferendis rerum sequi tenetur unde voluptas. Exercitationem illum saepe tempore ut. Accusamus
+		consectetur deleniti dicta, dolore dolores eligendi et in laboriosam laborum laudantium nam natus nesciunt
+		officiis pariatur praesentium provident quis rem reprehenderit, sed tenetur vitae voluptatum.</h1>
 </div>
 
 <style>
