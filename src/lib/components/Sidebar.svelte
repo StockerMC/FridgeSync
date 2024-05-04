3ecced4402f8e428484503cf44c5271011749128
Skilled5041 <skilled5041@gmail.com>
2024-05-04 19:57:51 -0400

    FRixes

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,15 +5,15 @@
 
 <AppRail>
 	<AppRailAnchor href="/dashboard" selected={$page.url.pathname === '/dashboard'} title="tile-1">
-		<span>Fridge</span>
+		<span class="text-lg">Fridge</span>
 	</AppRailAnchor>
 	<AppRailAnchor href="/dashboard/stats" selected={$page.url.pathname === '/dashboard/stats'} title="tile-2">
-		<span>Stats</span>
+		<span class="text-lg">Stats</span>
 	</AppRailAnchor>
 	<AppRailAnchor href="/recipes" selected={$page.url.pathname === '/recipes'} title="tile-2">
-		<span>Recipes</span>
+		<span class="text-lg">Recipes</span>
 	</AppRailAnchor>
 	<AppRailAnchor href="/groceries" selected={$page.url.pathname === '/groceries'} title="tile-2">
-		<span>Groceries</span>
+		<span class="text-md">Groceries</span>
 	</AppRailAnchor>
 </AppRail>
