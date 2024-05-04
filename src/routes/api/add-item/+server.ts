64515dd7ffcb7d24ef941e86fa7075ff0118d6b9
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-05-04 13:07:40 -0400

    Testing save item functionality

--- src/routes/api/add-item/+server.ts
+++ src/routes/api/add-item/+server.ts
@@ -1,10 +1,29 @@
 
+import type { Database } from "$lib/database-types";
 import {openai} from "$lib/openai";
+import { supabase } from "$lib/supabaseClient";
+import type { SupabaseClient } from "@supabase/supabase-js";
 import type { RequestHandler } from "@sveltejs/kit";
 
+async function saveItem(supabase: SupabaseClient<Database>, item: Database['public']['Tables']['fridge']['Insert']) {
+    // const supabase = createClientComponentClient()
+    const { data, error } = await supabase
+        .from('fridge')
+        .insert(item)
+        .select()
+        .single();
+    return { data, error };
+}
+
+
 // name, type, quantity, healthy, calories
-export const GET: RequestHandler = async ({request}) => {
-	//let { image } = await request.json();
+export const POST: RequestHandler = async ({request}) => {
+    const formData = await request.formData();
+    const base64_image_url = formData.get('image') as string;
+	if (!base64_image_url) {
+		return new Response('error');
+	}
+	console.log('a' + base64_image_url);
 	//const base64_image = Buffer.from(image, "base64").toString("base64");
 	const result = await openai.chat.completions.create({
 		model: "gpt-4-turbo",
@@ -14,19 +33,27 @@ export const GET: RequestHandler = async ({request}) => {
 				content: [
 					{
 						type: "text",
-						text: "What's in this image?"
+						text: "What food/ingredient is in this image? Answer ONLY in the following format, with \\n meaning to move to a new line: Name\nType"
 					},
-					/*{
+					{
 						type: "image_url",
 						image_url: {
-							url: `data:image/jpeg;base64, ${base64_image}`
+							url: base64_image_url
 						}
-					}*/
+					}
 				]
 			}
 		],
 		max_tokens: 30
 	});
+
+	const { data, error } = await saveItem(supabase, {
+		'name': 'Carrot',
+		'type': 'Vegetable'
+	});
+
+	console.log(data, error);
+
 	return new Response(
         result.choices[0].message.content,
         {
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,12 +1,36 @@
-<script>
-    export let data;
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const toBase64: (file: File) => any = file => new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => resolve(reader.result);
+		reader.onerror = reject;
+	});
+
+
+	onMount(() => {
+		document.getElementsByTagName('button')[0].addEventListener('click', async () => {
+	// @ts-ignore
+	let a = document.getElementsByTagName('input')[0].files[0];
+	console.log(a)
+	// TODO: save image to database
+  const data = new FormData();
+  // @ts-ignore
+  
+  data.append('image', await toBase64(a));
+  const response = await fetch(`/api/add-item`, {
+                method: 'POST',
+                body: data
+            })
+  console.log(await response.text())
+	})
+})
+
+
 </script>
 
-<ul>
-	<li>
-		test
-	</li>
-	{#each data.fridge as item}
-		<li>{item.name}</li>
-	{/each}
-</ul>
+<input type="file"/>Test
+<button>
+Submit
+</button>
