f200b220967b21e73480bcdff4314a0f2bbe61ca
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-05-04 13:22:53 -0400

    Display real data

--- src/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export type ItemType = {
-	  name: string;
-	  type: string;
-	  quantity:number;
-	  healthy: boolean;
-	  calories: number;
-};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,35 +9,39 @@
 		reader.onerror = reject;
 	});
 
+	export let data;
 
-	onMount(() => {
-		document.getElementsByTagName('button')[0].addEventListener('click', async () => {
-			// @ts-ignore
-			let a = document.getElementsByTagName('input')[0].files[0];
-			console.log(a)
-			// TODO: save image to database
-			const data = new FormData();
-			// @ts-ignore
+	// onMount(() => {
+	// 	document.getElementsByTagName('button')[0].addEventListener('click', async () => {
+	// 		// @ts-ignore
+	// 		let a = document.getElementsByTagName('input')[0].files[0];
+	// 		console.log(a)
+	// 		// TODO: save image to database
+	// 		const data = new FormData();
+	// 		// @ts-ignore
 
-			data.append('image', await toBase64(a));
-			const response = await fetch(`/api/add-item`, {
-				method: 'POST',
-				body: data
-			})
-			console.log(await response.text())
-		})
-	})
+	// 		data.append('image', await toBase64(a));
+	// 		const response = await fetch(`/api/add-item`, {
+	// 			method: 'POST',
+	// 			body: data
+	// 		})
+	// 		console.log(await response.text())
+	// 	})
+	// })
 </script>
 
-<input type="file"/>Test
+<input type="file"/>
 <div class="p-3">
 	<div class="flex mb-2">
 		<button type="button" class="btn variant-filled mr-auto"><span class="font-extrabold text-xl pr-1">+</span>add</button>
 		<button class="btn variant-filled ml-auto">⧩</button>
 	</div>
 	<div class="grid grid-cols-4 gap-6">
-		{#each {length: 7} as _, i}
+		<!-- {#each {length: 7} as _, i}
 			<Item item={{name: "testName", type:"testType", calories:1000, healthy:false, quantity: 2}} />
+		{/each} -->
+		{#each data.fridge as item}
+			<Item item={item}>{item.name}</Item>
 		{/each}
 	</div>
 </div>
