bf6aa36fb757010ff29b40d763fb4d7f5483f803
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-05-04 18:26:04 -0400

    Delete code

---
+++ src/routes/api/delete-item/+server.ts
@@ -0,0 +1,38 @@
+
+import type { Database } from "$lib/database-types";
+import { supabase } from "$lib/supabaseClient";
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { RequestHandler } from "@sveltejs/kit";
+
+async function deleteItem(supabase: SupabaseClient<Database>, id: number) {
+    // const supabase = createClientComponentClient()
+    const { data, error } = await supabase
+    .from('fridge')
+    .delete()
+    .eq('id', id);
+
+    await supabase
+        .storage
+        .from('fridge')
+        .remove([`${id}.png`]);
+    return { data, error };
+}
+
+
+// name, type, quantity, healthy, calories
+export const POST: RequestHandler = async ({request}) => {
+    const { searchParams } = new URL(request.url);
+	const id = Number.parseInt(searchParams.get('id') || '-1');
+    
+	const { data, error } = await deleteItem(supabase, id);
+
+	return new Response(
+        'Good',
+        {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST',
+            }
+        }
+    );
+};
